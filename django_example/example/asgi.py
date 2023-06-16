11ace201e880b18ba574f64c093dc47028b9ad1b
Viktor Babchanik <vbabchanik@ucdavis.edu>
2023-06-15 20:58:44 -0700

    Make bug fixes

--- django_example/example/asgi.py
+++ django_example/example/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testdramatiq project.
+ASGI config for dramatiq_kafka project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testdramatiq.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dramatiq_kafka.settings")
 
 application = get_asgi_application()
